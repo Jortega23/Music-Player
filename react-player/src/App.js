5bb875791979e07577e9fef29c1b730fc549e3a6
Jortega23 <jortegs23@gmail.com>
2020-11-16 20:48:22 -0800

    React-Player WIP 11/16
    Set up state for song, time, duration
    Update the UI to match the state

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './styles/app.scss'
 //Adding components
 import Player from './components/Player';
 import Song from './components/Song';
+//Import util.js
+import data from './util.js'
 
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] =useState(false);
+   
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong}/>
     </div>
   );
 }
