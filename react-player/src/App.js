f3fb41b7abd20bcc4f5d94dd03b40329feffc098
Jortega23 <jortegs23@gmail.com>
2020-11-23 16:31:40 -0800

    Wip 11/23
    stying player

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -6,7 +6,7 @@ import Song from './components/Song';
 import Library from './components/Library'
 import Nav from './components/Nav'
 //Import util.js
-import data from './util.js'
+import data from './data.js'
 
 function App() {
   //If we need to target a specific html element
@@ -22,13 +22,18 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
 
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime
     const duration = e.target.duration
-    setSongInfo({...songInfo, currentTime: current, duration: duration})
+    //calculate percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({...songInfo, currentTime: current, duration: duration, animationPercentage: animation});
   };
    
   return (
@@ -44,6 +49,7 @@ function App() {
         songInfo={songInfo}
         songs={songs}
         setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
       />
       <Library 
         audioRef={audioRef} 
--- react-player/src/components/Player.js
+++ react-player/src/components/Player.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faAngleLeft,faPause, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import {playAudio} from '../util'
 
-const Player = ({ audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, songInfo, songs, setCurrentSong }) => {
+const Player = ({ audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, songInfo, songs, setCurrentSong, setSongs }) => {
+
+    //use effect
+    useEffect(() => {
+    const newSongs = songs.map((song) => {
+      if(song.id === currentSong.id){
+        return{
+          ...song,
+          active: true,
+        }
+      }else{
+        return{
+          ...song,
+          active: false,
+        }
+      }
+    });
+    setSongs(newSongs);
+  }, [currentSong]);
 
     //eventhandlers------------------------------------------>
     const playSongHandler = () => {
@@ -29,15 +48,29 @@ const Player = ({ audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, s
     const skipDragHandler = (direction) => {
       let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
       if(direction === 'skip-forward') {
-        setCurrentSong(songs[currentIndex + 1]);
+        setCurrentSong(songs[(currentIndex + 1) % songs.length]);
       }
-    }
+      if(direction ==='skip-back'){
+        if((currentIndex - 1) % songs.length === -1){
+          setCurrentSong(songs[songs.length - 1]);
+          playAudio(isPlaying, audioRef);
+          return
+        }
+        setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      }
+      playAudio(isPlaying, audioRef);
+    };
+    //Add styles
+    const trackAnim = {
+      transform: `translateX(${songInfo.animationPercentage}%)`
+    };
 
 
     return (
       <div className='player'>
         <div className='time-control'>
           <p>{getTime(songInfo.currentTime)}</p>
+          <div style={{background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})`}}  className="track">
           <input 
             min={0} 
             max={songInfo.duration || 0} 
@@ -45,7 +78,9 @@ const Player = ({ audioRef, currentSong, isPlaying, setIsPlaying, setSongInfo, s
             onChange={dragHandler} 
             type='range' 
           />
-          <p>{getTime(songInfo.duration)}</p>
+          <div className="animate-track" style={trackAnim}></div>
+          </div>
+          <p>{songInfo.duration ? getTime(songInfo.duration) : '00:00'}</p>
         </div>
         
         <div className="play-control">
